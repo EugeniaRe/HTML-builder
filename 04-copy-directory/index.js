65374f9538f80d9395a683ad5b1424ee7b4275f7
EugeniaRe <evrepni@gmail.com>
2024-01-23 20:38:41 +0500

    refactor: add seprate function for copying folder in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,39 +4,30 @@ const path = require('path');
 const pathToFolder = path.join(__dirname, 'files');
 const copyDir = path.join(__dirname, 'files-copy');
 
+function copyFiles() {
+  fs.mkdir(copyDir, { recursive: true }, (err) => {
+    if (err) throw err;
+    fs.readdir(pathToFolder, { withFileTypes: true }, (err, files) => {
+      files.forEach((file) => {
+        fs.copyFile(
+          `${path.join(file.path, file.name)}`,
+          `${path.join(copyDir, file.name)}`,
+          (err) => {
+            if (err) throw err;
+          },
+        );
+      });
+    });
+  });
+}
+
 fs.exists(copyDir, (ex) => {
   if (ex) {
     fs.rm(copyDir, { recursive: true }, (err) => {
       if (err) throw err;
-      fs.mkdir(copyDir, { recursive: true }, (err) => {
-        if (err) throw err;
-        fs.readdir(pathToFolder, { withFileTypes: true }, (err, files) => {
-          files.forEach((file) => {
-            fs.copyFile(
-              `${path.join(file.path, file.name)}`,
-              `${path.join(copyDir, file.name)}`,
-              (err) => {
-                if (err) throw err;
-              },
-            );
-          });
-        });
-      });
+      copyFiles();
     });
   } else {
-    fs.mkdir(copyDir, { recursive: true }, (err) => {
-      if (err) throw err;
-      fs.readdir(pathToFolder, { withFileTypes: true }, (err, files) => {
-        files.forEach((file) => {
-          fs.copyFile(
-            `${path.join(file.path, file.name)}`,
-            `${path.join(copyDir, file.name)}`,
-            (err) => {
-              if (err) throw err;
-            },
-          );
-        });
-      });
-    });
+    copyFiles();
   }
 });
