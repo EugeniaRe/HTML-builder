c6b5cc9a8cd1367d63823abb2262cefd3df4f2d1
EugeniaRe <evrepni@gmail.com>
2024-01-23 20:32:50 +0500

    fix: add condition for existing files-copy in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,20 +1,42 @@
 const fs = require('fs');
 const path = require('path');
 
-const copyDir = path.join(__dirname, 'files-copy');
-fs.mkdir(copyDir, { recursive: true }, (err) => {
-  if (err) throw err;
-});
-
 const pathToFolder = path.join(__dirname, 'files');
-fs.readdir(pathToFolder, { withFileTypes: true }, (err, files) => {
-  files.forEach((file) => {
-    fs.copyFile(
-      `${path.join(file.path, file.name)}`,
-      `${path.join(copyDir, file.name)}`,
-      (err) => {
+const copyDir = path.join(__dirname, 'files-copy');
+
+fs.exists(copyDir, (ex) => {
+  if (ex) {
+    fs.rm(copyDir, { recursive: true }, (err) => {
+      if (err) throw err;
+      fs.mkdir(copyDir, { recursive: true }, (err) => {
         if (err) throw err;
-      },
-    );
-  });
+        fs.readdir(pathToFolder, { withFileTypes: true }, (err, files) => {
+          files.forEach((file) => {
+            fs.copyFile(
+              `${path.join(file.path, file.name)}`,
+              `${path.join(copyDir, file.name)}`,
+              (err) => {
+                if (err) throw err;
+              },
+            );
+          });
+        });
+      });
+    });
+  } else {
+    fs.mkdir(copyDir, { recursive: true }, (err) => {
+      if (err) throw err;
+      fs.readdir(pathToFolder, { withFileTypes: true }, (err, files) => {
+        files.forEach((file) => {
+          fs.copyFile(
+            `${path.join(file.path, file.name)}`,
+            `${path.join(copyDir, file.name)}`,
+            (err) => {
+              if (err) throw err;
+            },
+          );
+        });
+      });
+    });
+  }
 });
